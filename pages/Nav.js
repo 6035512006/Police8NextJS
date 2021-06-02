ded22e94bbc1baf15be2e3ebfadb08e73c4fddfd
6035512006 <6035512006@psu.ac.th>
2021-06-02 12:51:02 +0700

    first commit

---
+++ pages/Nav.js
@@ -0,0 +1,20 @@
+function Nav() {
+    return <div>
+
+        <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <a className="navbar-brand" href="#">Police 8</a>
+                </div>
+                <ul className="nav navbar-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Page 1</a></li>
+                    <li><a href="#">Page 2</a></li>
+                    <li><a href="#">Page 3</a></li>
+                </ul>
+            </div>
+        </nav>
+
+    </div>
+}
+export default Nav;
